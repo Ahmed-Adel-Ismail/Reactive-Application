51eb7724f71d66c01818acb67b296c69046f7247
Ahmed Adel Ismail <ahm3d.ad3l@gmail.com>
2018-05-15 14:27:57 +0200

    added more operators

--- app/src/main/java/com/reactive/app/MainActivity.kt
+++ app/src/main/java/com/reactive/app/MainActivity.kt
@@ -2,15 +2,18 @@ package com.reactive.app
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.AutoCompleteTextView
+import android.widget.ListAdapter
+import io.reactivex.Observable
 
 class MainActivity : Activity() {
+
+    val autoComplete: AutoCompleteTextView by lazy { AutoCompleteTextView(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
+        Observable.just(1)
     }
 }
 
